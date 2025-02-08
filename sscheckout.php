b8faef50202ba8446193d914b882539ee3bcb478
mach7 <tyson@mach7enterprises.com>
2025-02-08 09:23:03 -0700

    init

---
+++ sscheckout.php
@@ -0,0 +1,10 @@
+<?php 
+/* 
+Plugin Name: Simple Stripe Checkout 
+Description: A chat assistant for the WordPress dashboard. 
+Version: 1.0.4.1 
+Author: Tyson Brooks 
+Organization: Frostline Works 
+Author URI: https://frostlineworks.com 
+Tested up to: 6.7.1 
+*/ 
